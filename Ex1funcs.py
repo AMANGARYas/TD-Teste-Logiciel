2ad49e377c53339102ae612401714d62a0fd5c82
YounesChefou <younes.chefou@gmail.com>
2024-01-10 18:15:59 +0100

    test Ex1 Q1

---
+++ Ex1funcs.py
@@ -0,0 +1,18 @@
+
+
+def max_three(lst):
+	pass
+
+def min_n(lst, n):
+	pass
+	
+def is_prime(n):
+	pass
+
+def is_arithmetic_sequence(sequence):
+	pass
+
+def is_geometric_sequence(sequence):
+	pass
+
+
