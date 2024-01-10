d13b6fb0a7d47209ffaaf79ebe412e04425481fd
YounesChefou <younes.chefou@gmail.com>
2024-01-10 18:28:27 +0100

    test Ex1 Q2

--- Ex1tests_funcs.py
+++ Ex1tests_funcs.py
@@ -10,6 +10,14 @@ class TestFuncs(unittest.TestCase):
         self.assertEqual(Ex1funcs.max_three([-5, -2, -8, -1, -7]), [-1, -2, -5])
         self.assertEqual(Ex1funcs.max_three([]), [])
 
+    def test_min_n(self):
+        self.assertEqual(Ex1funcs.min_n([5, 2, 8, 1, 7], 3), [1, 2, 5])
+        self.assertEqual(Ex1funcs.min_n([10, 20, 30, 40, 50], 2), [10, 20])
+        self.assertEqual(Ex1funcs.min_n([1, 2, 3], 1), [1])
+        self.assertEqual(Ex1funcs.min_n([-5, -2, -8, -1, -7], 4), [-8, -7, -5, -2])
+        self.assertEqual(Ex1funcs.min_n([], 5), [])
+
+
              
 
 
