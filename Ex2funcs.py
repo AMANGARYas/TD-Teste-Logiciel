ca3a3aa17f238600b4a63d221740c13f9e192a81
YounesChefou <younes.chefou@gmail.com>
2024-01-10 19:08:15 +0100

    test FIFO

---
+++ Ex2funcs.py
@@ -0,0 +1,13 @@
+class FIFO:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, item):
+    	pass
+
+    def dequeue(self):
+    	pass
+
+    def is_empty(self):
+    	pass
+
